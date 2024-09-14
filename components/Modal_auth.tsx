60c9755887dddef901163e55f15ffa484d7192f7
Aayush Kumar Bhat <kumarbhataayush@gmail.com>
2024-09-14 11:21:31 +0530

    R_CHANGES

--- components/Modal_auth.tsx
+++ components/Modal_auth.tsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
-import { Dialog, DialogTrigger, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { EyeIcon } from "lucide-react";
+import {MessageCircle} from "lucide-react"
 
 const ModalAuth = () => {
   const [activeMode, setActiveMode] = useState("login");
@@ -12,11 +19,13 @@ const ModalAuth = () => {
       <div className="flex justify-center mb-2">
         <Button
           className={`${
-            activeMode === "signup" ? "bg-[#DFC38A] text-black" : "bg-transparent text-[#DFC38A] hover:bg-[#BA9359]"
-          } px-6 py-2 rounded-md`}
-          onClick={() => setActiveMode("signup")}
+            activeMode === "login"
+              ? "text-[#DFC38A] bg-black "
+              : "bg-transparent text-[#DFC38A] hover:bg-[#BA9359]"
+          } px-6 py-2 rounded-md `}
+          onClick={() => setActiveMode("login")}
         >
-          Signup
+          Login
         </Button>
       </div>
 
@@ -25,40 +34,70 @@ const ModalAuth = () => {
       </DialogHeader> */}
 
       {activeMode === "login" ? (
-        <>
-         <div className="w-full h-full ">
-          <h3 className="text-2xl font-bold mb-3 text-center p-3 font-serif  ">Welcome Back</h3>
-          <form>
-            <div className="mb-3">
-              <label>Email or Phone Number</label>
-              <input type="text" className="w-full p-2 mt-1 bg-black    border border-[#DFC38A] rounded-md " />
-            </div>
-            <div className="mb-3">
-              <label>Password</label>
-              <input type="password" className="w-full p-2 mt-1 bg-[#BA9359] rounded-md" />
-            </div>
-            <Button className="bg-[#DFC38A] text-black mt-3 w-full">Log In</Button>
-          </form>
-        </div>
-        </>
-       
+        <section className="h-full w-full  mt-[10rem]">
+          <h3 className="text-[3rem] font-bold  text-center p-2 font-serif  ">
+            Welcome Back
+          </h3>
+          <p className="text-[1.25rem]   text-center  font-serif">
+            We’re so excited to see you again!
+          </p>
+          <div className="w-full h-full flex justify-center mt-[3rem]">
+            <form>
+              <div className="mb-3 w-full ">
+                <label>Email or Phone Number</label>
+               <div className="flex items-center w-full ">
+               <input
+                  type="text"
+                  className="w-full p-2 mt-1 bg-black   border border-[#DFC38A] rounded-md "
+                />
+                <MessageCircle className="ml-[1rem]"/>
+               </div>
+              </div>
+              <div className="mb-3 bg-black">
+                <label>Password</label>
+                {/* password */}
+                <div className="w-full h-full flex items-center">
+                  <input
+                    type="password"
+                    className="w-full p-2 mt-1 bg-black  border border-[#DFC38A] rounded-md"
+                  />
+                  <EyeIcon className="bg-red text-center h-full ml-[1rem]" />
+                </div>
+              </div>
+              <Button className="bg-[#DFC38A] text-black mt-3 w-full">
+                Log In
+              </Button>
+            </form>
+          </div>
+        </section>
       ) : (
         <div>
           <h3 className="text-2xl font-bold mb-3">Join NyayaMitra</h3>
           <form>
             <div className="mb-3">
               <label>Email</label>
-              <input type="email" className="w-full p-2 mt-1 bg-[#BA9359] rounded-md" />
+              <input
+                type="email"
+                className="w-full p-2 mt-1 bg-[#BA9359] rounded-md"
+              />
             </div>
             <div className="mb-3">
               <label>Username</label>
-              <input type="text" className="w-full p-2 mt-1 bg-[#BA9359] rounded-md" />
+              <input
+                type="text"
+                className="w-full p-2 mt-1 bg-[#BA9359] rounded-md"
+              />
             </div>
             <div className="mb-3">
               <label>Password</label>
-              <input type="password" className="w-full p-2 mt-1 bg-[#BA9359] rounded-md" />
+              <input
+                type="password"
+                className="w-full p-2 mt-1 bg-[#BA9359] rounded-md"
+              />
             </div>
-            <Button className="bg-[#DFC38A] text-black mt-3 w-full">Create Account</Button>
+            <Button className="bg-[#DFC38A] text-black mt-3 w-full">
+              Create Account
+            </Button>
           </form>
         </div>
       )}
@@ -71,16 +110,18 @@ const ModalAuth = () => {
       <div className="absolute top-0 left-0 right-0 flex justify-center mt-2 z-10">
         <Button
           className={`${
-            activeMode === "login" ? "bg-[#DFC38A] text-black" : "bg-transparent text-[#DFC38A] hover:bg-[#BA9359]"
+            activeMode === "signup"
+              ? "bg-[#DFC38A] text-black"
+              : "bg-transparent text-[#DFC38A] hover:bg-[#BA9359]"
           } px-6 py-2 rounded-md`}
-          onClick={() => setActiveMode("login")}
-        >
-          Login 
-        </Button>
+          onClick={() => setActiveMode("signup")}
+        >Signup</Button>
       </div>
 
       <Image
-        src={activeMode === "login" ? "/images/login.png" : "/images/SignUp.png"}
+        src={
+          activeMode === "login" ? "/images/login.png" : "/images/SignUp.png"
+        }
         alt={activeMode === "login" ? "Login" : "Signup"}
         layout="fill"
         objectFit="cover"
@@ -92,7 +133,7 @@ const ModalAuth = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <div className="inline-block">
+        <div >
           <Button className="font-semibold text-lg text-black bg-[#DFC38A] hover:bg-[#BA9359] rounded-md">
             Get Started
             <Image
@@ -105,7 +146,7 @@ const ModalAuth = () => {
           </Button>
         </div>
       </DialogTrigger>
-      <DialogContent className="p-0 w-[90vw] h-[90vh] bg-transparent border-none max-w-none flex gap-0">
+      <DialogContent className="p-0 w-[90vw] h-[90vh] bg-transparent border-none max-w-none  flex  gap-0">
         {/* Modal Content */}
         {activeMode === "login" ? (
           <>
