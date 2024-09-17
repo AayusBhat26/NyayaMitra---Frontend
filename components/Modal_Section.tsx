4a9e36f1b23afcdc2c9e940d00a19a18b04238e4
Aayush Kumar Bhat <kumarbhataayush@gmail.com>
2024-09-17 11:38:01 +0530

    deployment changes

--- components/Modal_Section.tsx
+++ components/Modal_Section.tsx
@@ -14,12 +14,12 @@ export const ModalSection = () => {
   const [mouseDownAt, setMouseDownAt] = useState(0);
   const [prevPercentage, setPrevPercentage] = useState(0);
   const [percentage, setPercentage] = useState(0);
-  const imageArray = ["/images/chatbot_image.jpg","/images/lawyer_dashboard.jpg","/images/traffic_challan.png", ]
+  const imageArray = ["/images/chatbot_image.jpg", "/images/lawyer_dashboard.jpg", "/images/traffic_challan.png"];
   const imageObject = {
     "Chatbot": imageArray[0],
     "Lawyer Dashboard": imageArray[1],
     "Traffic Challan": imageArray[2],
-  }
+  };
 
   // Handle the drag functionality
   useEffect(() => {
@@ -32,7 +32,10 @@ export const ModalSection = () => {
 
       const mouseDelta = mouseDownAt - e.clientX;
       const maxDelta = window.innerWidth / 2;
-      const nextPercentage = Math.max(Math.min(prevPercentage + (mouseDelta / maxDelta) * 100, 0), -100);
+      const nextPercentage = Math.max(
+        Math.min(prevPercentage + (mouseDelta / maxDelta) * 100, 0),
+        -100
+      );
 
       setPercentage(nextPercentage);
       trackRef.current.style.transform = `translate(${nextPercentage}%, -50%)`;
@@ -61,7 +64,7 @@ export const ModalSection = () => {
       trackRef.current.style.transform = `translate(${nextPercentage}%, -50%)`;
     }
   };
-  
+
   const handleMoveRight = () => {
     const nextPercentage = Math.min(percentage + 20, 0); // Increase percentage to move right
     setPercentage(nextPercentage);
@@ -69,7 +72,6 @@ export const ModalSection = () => {
       trackRef.current.style.transform = `translate(${nextPercentage}%, -50%)`;
     }
   };
-  
 
   return (
     <Dialog>
@@ -78,7 +80,7 @@ export const ModalSection = () => {
           Services
         </Button>
       </DialogTrigger>
-      <DialogContent className="  min-w-full h-full bg-black bg-opacity-[0.2] m-0 overflow-hidden flex flex-col justify-center items-center">
+      <DialogContent className="min-w-full h-full bg-black bg-opacity-[0.2] m-0 overflow-hidden flex flex-col justify-center items-center">
         <div
           ref={trackRef}
           className="flex gap-4 transition-transform duration-1000"
@@ -86,19 +88,19 @@ export const ModalSection = () => {
           style={{ transform: `translate(${percentage}%, -50%)` }}
         >
           {Object.entries(imageObject).map(([name, src]) => (
-            <div key={name} className="h-full w-full relative  overflow-hidden">
-              <div key={name} className="flex flex-col items-center   ">
-            <Image
-              src={src}
-              key={name}
-              alt={name}
-              width={400}
-              height={300}
-              className="w-[50vmin] h-[56vmin] object-cover object-center rounded-md hover:scale-110 transition brightness-50 hover:brightness-75"
-            />
-            {/* <div className="inset-0 h-full w-full absolute top-0 bottom-0 bg-black opacity-40 hover:opacity-20"></div> */}
-            <p className="text-white text-lg mt-2 absolute bottom-0 bg-black bg-opacity-75 w-full text-center p-2 ">{name}</p>
-          </div>
+            <div key={name} className="h-full w-full relative overflow-hidden">
+              <div className="flex flex-col items-center">
+                <Image
+                  src={src}
+                  alt={name}
+                  width={400}
+                  height={300}
+                  className="w-[50vmin] h-[56vmin] object-cover object-center rounded-md hover:scale-110 transition brightness-50 hover:brightness-75"
+                />
+                <p className="text-white text-lg mt-2 absolute bottom-0 bg-black bg-opacity-75 w-full text-center p-2">
+                  {name}
+                </p>
+              </div>
             </div>
           ))}
         </div>
